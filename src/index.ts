86a98c3a3dcac8ab66e17aa5a1d4a64b295fef2b
Mike Lippert <mlippert255@gmail.com>
2023-02-02 14:32:22 -0500

    Hello World
    Entry Point source file which builds and runs
    ```
    make build
    make run
    ```
    also `make run-dev`
    
    and `make lint` shows a single warning:
    ```
    /home/mjl/Projects/riff/_aux/mysql-query/src/index.ts
      15:1  warning  Unexpected console statement  no-console
    
    ✖ 1 problem (0 errors, 1 warning)
    ```

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/* ******************************************************************************
+ * index.ts                                                                     *
+ * *************************************************************************/ /**
+ *
+ * @fileoverview Application Entry Point
+ *
+ * Created on       February 2, 2023
+ * @author          Michael Jay Lippert
+ *
+ * @copyright (c) 2023 Michael Jay Lippert,
+ *            MIT License (see https://opensource.org/licenses/MIT)
+ *
+ * ******************************************************************************/
+
+console.log('Hello World');
